dc37314c5c6077884e594e72822937065d719cbb
amolc <amol.chawathe@fountaintechies.com>
2017-10-19 09:18:29 +0530

    app name added

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,8 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "in.edelworks.sharedpreferences.SharedPreferences",
+    "file": "plugins/in.edelworks.sharedpreferences/www/sharedpreferences.js",
+    "pluginId": "in.edelworks.sharedpreferences",
+    "clobbers": [
+      "sharedpreferences"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+  "in.edelworks.sharedpreferences": "0.1.0",
   "cordova-plugin-whitelist": "1.3.2"
 };
 // BOTTOM OF METADATA
