d8a6206425008573c1cf9aee8febf084ddbc70ad
Nadeem <nadyshaikh@gmail.com>
2017-11-28 15:13:10 +0530

    updating

--- www/js/app.js
+++ www/js/app.js
@@ -11,12 +11,12 @@ angular.module('starter', ['ionic','starter.controllers', 'starter.services','ng
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-   /* if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
-    if (window.StatusBar) {
+    /*if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }*/
